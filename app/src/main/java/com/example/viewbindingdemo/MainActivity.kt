5c17cf3eb3a7207a969b2f305e2e99fcb978c3a7
halil.olcay <holcay92@gmail.com>
2022-12-19 23:35:02 +0300

    default code is changed to binding and apply function is used

--- app/src/main/java/com/example/viewbindingdemo/MainActivity.kt
+++ app/src/main/java/com/example/viewbindingdemo/MainActivity.kt
@@ -5,20 +5,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.viewbindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val messageView = findViewById<TextView>(R.id.tvName)
-        val messageInput = findViewById<EditText>(R.id.etName)
-        val submitButton = findViewById<Button>(R.id.btnSubmit)
-
-
-        submitButton.setOnClickListener {
-            val name = messageInput.text.toString()
-            messageView.text ="Hello! $name"
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val name = etName.text.toString()
+                tvName.text = "Hello! $name"
+                etName.setText("")
+            }
         }
+
     }
 }
